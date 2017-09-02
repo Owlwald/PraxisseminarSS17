0d4c6b313eb0b81ebbd299b756d0b27d1256c550
Gina Maria Wolf <ginamariawolf@gmail.com>
2017-09-02 11:42:24 +0200

    controller for single art page

---
+++ dicamu/src/js/controllers/art.controller.js
@@ -0,0 +1,26 @@
+(function () {
+    'use strict';
+
+    angular
+        .module('dnb.art')
+        .controller('ArtCtrl', ArtController);
+
+    ////////////////////////////////////////
+
+    ArtController.$inject = [
+        '$scope',
+    ];
+
+    function ArtController($scope) {
+
+        $scope.currentUser = null;
+        $scope.currentArt = null;
+        activate();
+
+        function activate() {
+            //not implemented yet:
+            //$scope.currentUser = AppContext.getActiveUser();
+            //$scope.currentArt = AppContext.getActiveArt();
+        }
+    }
+})();
