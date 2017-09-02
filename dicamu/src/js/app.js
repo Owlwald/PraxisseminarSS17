1ae372920af14552f4f23a95e8b90e24a75a55e3
Gina Maria Wolf <ginamariawolf@gmail.com>
2017-09-02 15:06:24 +0200

    organized

--- dicamu/src/js/app.js
+++ dicamu/src/js/app.js
@@ -1,16 +1,30 @@
-angular.module('Dicamu', [
-  'ngRoute',
-  'mobile-angular-ui',
-  'Dicamu.controllers.Main'
-])
+'use strict';
 
-.config(function ($routeProvider) {
-    $routeProvider.when('/', {
-        templateUrl: 'home.html',
+
+
+var app = angular.module('Dicamu', [
+    "ngRoute",
+    "mobile-angular-ui",
+    'Dicamu.controllers.Main'
+]);
+
+app.config(function ($routeProvider) {
+    $routeProvider
+        .when('/', {
+            templateUrl: "home.html",
+            reloadOnSearch: false
+        })
+        .when("/art", {
+            templateUrl: "art.html",
+            reloadOnSearch: false
+        });
+
+    $routeProvider.when('/catalog', {
+        templateUrl: "catalog.html",
         reloadOnSearch: false
     });
-    $routeProvider.when('/art', {
-        templateUrl: 'art.html',
+    $routeProvider.when('/catalogues', {
+        templateUrl: "catalogues.html",
         reloadOnSearch: false
     });
 });
