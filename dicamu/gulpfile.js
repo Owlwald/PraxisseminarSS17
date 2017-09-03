98081f177e69059ba27370713938b626a5368c36
Gina Maria Wolf <ginamariawolf@gmail.com>
2017-09-04 00:06:28 +0200

    art is scrollable

--- dicamu/gulpfile.js
+++ dicamu/gulpfile.js
@@ -8,51 +8,52 @@
 // Please use config.js to override these selectively:
 
 var config = {
-  dest: 'www',
-  cordova: true,
-  less: {
-    src: [
+    dest: 'www',
+    cordova: true,
+    less: {
+        src: [
       './src/less/app.less', './src/less/responsive.less'
     ],
-    paths: [
+        paths: [
       './src/less', './bower_components'
     ]
-  },
-  vendor: {
-    js: [
+    },
+    vendor: {
+        js: [
       './bower_components/angular/angular.js',
       './bower_components/angular-route/angular-route.js',
-      './bower_components/mobile-angular-ui/dist/js/mobile-angular-ui.js'
+      './bower_components/mobile-angular-ui/dist/js/mobile-angular-ui.js',
+      './bower_components/mobile-angular-ui/dist/js/mobile-angular-ui.gestures.js'
     ],
 
-    css: {
-      prepend: [],
-      append: [],
-    },
+        css: {
+            prepend: [],
+            append: [],
+        },
 
-    fonts: [
+        fonts: [
       './bower_components/font-awesome/fonts/fontawesome-webfont.*'
     ]
-  },
+    },
 
-  server: {
-    host: '0.0.0.0',
-    port: '8000'
-  },
+    server: {
+        host: '0.0.0.0',
+        port: '8000'
+    },
 
-  weinre: {
-    httpPort:     8001,
-    boundHost:    'localhost',
-    verbose:      false,
-    debug:        false,
-    readTimeout:  5,
-    deathTimeout: 15
-  }
+    weinre: {
+        httpPort: 8001,
+        boundHost: 'localhost',
+        verbose: false,
+        debug: false,
+        readTimeout: 5,
+        deathTimeout: 15
+    }
 };
 
 if (require('fs').existsSync('./config.js')) {
-  var configFn = require('./config');
-  configFn(config);
+    var configFn = require('./config');
+    configFn(config);
 }
 
 /*-----  End of Configuration  ------*/
@@ -62,33 +63,33 @@ if (require('fs').existsSync('./config.js')) {
 =            Requiring stuffs            =
 ========================================*/
 
-var gulp           = require('gulp'),
-    seq            = require('run-sequence'),
-    connect        = require('gulp-connect'),
-    less           = require('gulp-less'),
-    uglify         = require('gulp-uglify'),
-    sourcemaps     = require('gulp-sourcemaps'),
-    cssmin         = require('gulp-cssmin'),
-    order          = require('gulp-order'),
-    concat         = require('gulp-concat'),
-    ignore         = require('gulp-ignore'),
-    rimraf         = require('gulp-rimraf'),
-    templateCache  = require('gulp-angular-templatecache'),
-    mobilizer      = require('gulp-mobilizer'),
-    ngAnnotate     = require('gulp-ng-annotate'),
-    replace        = require('gulp-replace'),
-    ngFilesort     = require('gulp-angular-filesort'),
-    streamqueue    = require('streamqueue'),
-    rename         = require('gulp-rename'),
-    path           = require('path');
+var gulp = require('gulp'),
+    seq = require('run-sequence'),
+    connect = require('gulp-connect'),
+    less = require('gulp-less'),
+    uglify = require('gulp-uglify'),
+    sourcemaps = require('gulp-sourcemaps'),
+    cssmin = require('gulp-cssmin'),
+    order = require('gulp-order'),
+    concat = require('gulp-concat'),
+    ignore = require('gulp-ignore'),
+    rimraf = require('gulp-rimraf'),
+    templateCache = require('gulp-angular-templatecache'),
+    mobilizer = require('gulp-mobilizer'),
+    ngAnnotate = require('gulp-ng-annotate'),
+    replace = require('gulp-replace'),
+    ngFilesort = require('gulp-angular-filesort'),
+    streamqueue = require('streamqueue'),
+    rename = require('gulp-rename'),
+    path = require('path');
 
 
 /*================================================
 =            Report Errors to Console            =
 ================================================*/
 
-gulp.on('error', function(e) {
-  throw(e);
+gulp.on('error', function (e) {
+    throw (e);
 });
 
 
@@ -97,14 +98,16 @@ gulp.on('error', function(e) {
 =========================================*/
 
 gulp.task('clean', function (cb) {
-  return gulp.src([
+    return gulp.src([
         path.join(config.dest, 'index.html'),
         path.join(config.dest, 'images'),
         path.join(config.dest, 'css'),
         path.join(config.dest, 'js'),
         path.join(config.dest, 'fonts')
-      ], { read: false })
-     .pipe(rimraf());
+      ], {
+            read: false
+        })
+        .pipe(rimraf());
 });
 
 
@@ -112,17 +115,17 @@ gulp.task('clean', function (cb) {
 =            Start a web server            =
 ==========================================*/
 
-gulp.task('connect', function() {
-  if (typeof config.server === 'object') {
-    connect.server({
-      root: config.dest,
-      host: config.server.host,
-      port: config.server.port,
-      livereload: true
-    });
-  } else {
-    throw new Error('Connect is not configured');
-  }
+gulp.task('connect', function () {
+    if (typeof config.server === 'object') {
+        connect.server({
+            root: config.dest,
+            host: config.server.host,
+            port: config.server.port,
+            livereload: true
+        });
+    } else {
+        throw new Error('Connect is not configured');
+    }
 });
 
 
@@ -131,8 +134,8 @@ gulp.task('connect', function() {
 ==============================================================*/
 
 gulp.task('livereload', function () {
-  gulp.src(path.join(config.dest, '*.html'))
-    .pipe(connect.reload());
+    gulp.src(path.join(config.dest, '*.html'))
+        .pipe(connect.reload());
 });
 
 
@@ -141,7 +144,7 @@ gulp.task('livereload', function () {
 =====================================*/
 
 gulp.task('images', function () {
-  return gulp.src('src/images/**/*')
+    return gulp.src('src/images/**/*')
         .pipe(gulp.dest(path.join(config.dest, 'images')));
 });
 
@@ -150,9 +153,9 @@ gulp.task('images', function () {
 =            Copy fonts            =
 ==================================*/
 
-gulp.task('fonts', function() {
-  return gulp.src(config.vendor.fonts)
-  .pipe(gulp.dest(path.join(config.dest, 'fonts')));
+gulp.task('fonts', function () {
+    return gulp.src(config.vendor.fonts)
+        .pipe(gulp.dest(path.join(config.dest, 'fonts')));
 });
 
 
@@ -160,17 +163,17 @@ gulp.task('fonts', function() {
 =            Copy html files to dest              =
 =================================================*/
 
-gulp.task('html', function() {
-  var inject = [];
-  if (typeof config.weinre === 'object') {
-    inject.push('<script src="http://'+config.weinre.boundHost+':'+config.weinre.httpPort+'/target/target-script-min.js"></script>');
-  }
-  if (config.cordova) {
-    inject.push('<script src="cordova.js"></script>');
-  }
-  gulp.src(['src/html/**/*.html'])
-  .pipe(replace('<!-- inject:js -->', inject.join('\n    ')))
-  .pipe(gulp.dest(config.dest));
+gulp.task('html', function () {
+    var inject = [];
+    if (typeof config.weinre === 'object') {
+        inject.push('<script src="http://' + config.weinre.boundHost + ':' + config.weinre.httpPort + '/target/target-script-min.js"></script>');
+    }
+    if (config.cordova) {
+        inject.push('<script src="cordova.js"></script>');
+    }
+    gulp.src(['src/html/**/*.html'])
+        .pipe(replace('<!-- inject:js -->', inject.join('\n    ')))
+        .pipe(gulp.dest(config.dest));
 });
 
 
@@ -180,23 +183,25 @@ gulp.task('html', function() {
 
 gulp.task('less', function () {
     return gulp.src(config.less.src).pipe(less({
-      paths: config.less.paths.map(function(p){
-        return path.resolve(__dirname, p);
-      })
-    }))
-    .pipe(mobilizer('app.css', {
-      'app.css': {
-        hover: 'exclude',
-        screens: ['0px']
-      },
-      'hover.css': {
-        hover: 'only',
-        screens: ['0px']
-      }
-    }))
-    .pipe(cssmin())
-    .pipe(rename({suffix: '.min'}))
-    .pipe(gulp.dest(path.join(config.dest, 'css')));
+            paths: config.less.paths.map(function (p) {
+                return path.resolve(__dirname, p);
+            })
+        }))
+        .pipe(mobilizer('app.css', {
+            'app.css': {
+                hover: 'exclude',
+                screens: ['0px']
+            },
+            'hover.css': {
+                hover: 'only',
+                screens: ['0px']
+            }
+        }))
+        .pipe(cssmin())
+        .pipe(rename({
+            suffix: '.min'
+        }))
+        .pipe(gulp.dest(path.join(config.dest, 'css')));
 });
 
 
@@ -206,19 +211,25 @@ gulp.task('less', function () {
 // - Orders ng deps automatically
 // - Precompile templates to ng templateCache
 
-gulp.task('js', function() {
-    streamqueue({ objectMode: true },
-      gulp.src(config.vendor.js),
-      gulp.src('./src/js/**/*.js').pipe(ngFilesort()),
-      gulp.src(['src/templates/**/*.html']).pipe(templateCache({ module: 'Dicamu' }))
-    )
-    .pipe(sourcemaps.init())
-    .pipe(concat('app.js'))
-    .pipe(ngAnnotate())
-    .pipe(uglify())
-    .pipe(rename({suffix: '.min'}))
-    .pipe(sourcemaps.write('.'))
-    .pipe(gulp.dest(path.join(config.dest, 'js')));
+gulp.task('js', function () {
+    streamqueue({
+                objectMode: true
+            },
+            gulp.src(config.vendor.js),
+            gulp.src('./src/js/**/*.js').pipe(ngFilesort()),
+            gulp.src(['src/templates/**/*.html']).pipe(templateCache({
+                module: 'Dicamu'
+            }))
+        )
+        .pipe(sourcemaps.init())
+        .pipe(concat('app.js'))
+        .pipe(ngAnnotate())
+        .pipe(uglify())
+        .pipe(rename({
+            suffix: '.min'
+        }))
+        .pipe(sourcemaps.write('.'))
+        .pipe(gulp.dest(path.join(config.dest, 'js')));
 });
 
 
@@ -227,13 +238,13 @@ gulp.task('js', function() {
 ===================================================================*/
 
 gulp.task('watch', function () {
-  if (typeof config.server === 'object') {
-    gulp.watch([config.dest + '/**/*'], ['livereload']);
-  }
-  gulp.watch(['./src/html/**/*'], ['html']);
-  gulp.watch(['./src/less/**/*'], ['less']);
-  gulp.watch(['./src/js/**/*', './src/templates/**/*', config.vendor.js], ['js']);
-  gulp.watch(['./src/images/**/*'], ['images']);
+    if (typeof config.server === 'object') {
+        gulp.watch([config.dest + '/**/*'], ['livereload']);
+    }
+    gulp.watch(['./src/html/**/*'], ['html']);
+    gulp.watch(['./src/less/**/*'], ['less']);
+    gulp.watch(['./src/js/**/*', './src/templates/**/*', config.vendor.js], ['js']);
+    gulp.watch(['./src/images/**/*'], ['images']);
 });
 
 
@@ -241,13 +252,13 @@ gulp.task('watch', function () {
 =            Starts a Weinre Server                 =
 ===================================================*/
 
-gulp.task('weinre', function() {
-  if (typeof config.weinre === 'object') {
-    var weinre = require('./node_modules/weinre/lib/weinre');
-    weinre.run(config.weinre);
-  } else {
-    throw new Error('Weinre is not configured');
-  }
+gulp.task('weinre', function () {
+    if (typeof config.weinre === 'object') {
+        var weinre = require('./node_modules/weinre/lib/weinre');
+        weinre.run(config.weinre);
+    } else {
+        throw new Error('Weinre is not configured');
+    }
 });
 
 
@@ -255,9 +266,9 @@ gulp.task('weinre', function() {
 =            Build Sequence            =
 ======================================*/
 
-gulp.task('build', function(done) {
-  var tasks = ['html', 'fonts', 'images', 'less', 'js'];
-  seq('clean', tasks, done);
+gulp.task('build', function (done) {
+    var tasks = ['html', 'fonts', 'images', 'less', 'js'];
+    seq('clean', tasks, done);
 });
 
 
@@ -265,18 +276,18 @@ gulp.task('build', function(done) {
 =            Default Task            =
 ====================================*/
 
-gulp.task('default', function(done){
-  var tasks = [];
+gulp.task('default', function (done) {
+    var tasks = [];
 
-  if (typeof config.weinre === 'object') {
-    tasks.push('weinre');
-  }
+    if (typeof config.weinre === 'object') {
+        tasks.push('weinre');
+    }
 
-  if (typeof config.server === 'object') {
-    tasks.push('connect');
-  }
+    if (typeof config.server === 'object') {
+        tasks.push('connect');
+    }
 
-  tasks.push('watch');
+    tasks.push('watch');
 
-  seq('build', tasks, done);
+    seq('build', tasks, done);
 });
--- dicamu/src/js/app.js
+++ dicamu/src/js/app.js
@@ -1,31 +1,35 @@
-'use strict';
+(function () {
+    'use strict';
 
-var app = angular.module('Dicamu', [
-    'ngRoute',
-    'mobile-angular-ui',
-    'Dicamu.controllers.Main'
-]);
+    /*global angular */
+    var app = angular.module('Dicamu', [
+        'ngRoute',
+        'mobile-angular-ui',
+        'mobile-angular-ui.gestures',
+        'Dicamu.controllers.Main'
+    ]);
 
-app.config(function ($routeProvider) {
-    $routeProvider
-        .when('/', {
-            templateUrl: "home.html",
-            reloadOnSearch: false
-        })
-        .when('/home', {
-            templateUrl: "home.html",
-            reloadOnSearch: false
-        })
-        .when('/my-catalogues', {
-            templateUrl: "my-catalogues.html",
-            reloadOnSearch: false
-        })
-        .when('/catalog', {
-            templateUrl: "catalog.html",
-            reloadOnSearch: false
-        })
-        .when('/art', {
-            templateUrl: "art.html",
-            reloadOnSearch: false
-        });
-});
+    app.config(function ($routeProvider) {
+        $routeProvider
+            .when('/', {
+                templateUrl: "home.html",
+                reloadOnSearch: false
+            })
+            .when('/home', {
+                templateUrl: "home.html",
+                reloadOnSearch: false
+            })
+            .when('/my-catalogues', {
+                templateUrl: "my-catalogues.html",
+                reloadOnSearch: false
+            })
+            .when('/catalog', {
+                templateUrl: "catalog.html",
+                reloadOnSearch: false
+            })
+            .when('/art', {
+                templateUrl: "art.html",
+                reloadOnSearch: false
+            });
+    });
+}());
