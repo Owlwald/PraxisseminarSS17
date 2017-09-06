bf7024866968da59a87e97a5c4824a713a0b648d
Gina Maria Wolf <ginamariawolf@gmail.com>
2017-09-07 00:14:36 +0200

    preparing for data from database

--- dicamu/src/js/app.js
+++ dicamu/src/js/app.js
@@ -1,13 +1,7 @@
 (function () {
     'use strict';
 
-
-    console.log("here i am")
-        /*global angular */
     var app = angular.module('dcm', [
-        'dcm.fire',
-        //'dcm.art',
-        //external components
         'ngRoute',
         'mobile-angular-ui',
         'mobile-angular-ui.gestures',
@@ -40,6 +34,23 @@
                 templateUrl: "art.html",
                 reloadOnSearch: false
             });
+    });
+
+    app.factory('fireBaseData', function ($firebase) {
+        var ref = new Firebase("https://dicamu-1ed50.firebaseio.com"),
+            refUsers = new Firebase("https://dicamu-1ed50.firebaseio.com/User"),
+            refMuseums = new Firebase("https://dicamu-1ed50.firebaseio.com/Museum");
+        return {
+            ref: function () {
+                return ref;
+            },
+            refUsers: function () {
+                return refUsers;
+            },
+            refMuseums: function () {
+                return refMuseums;
+            }
+        }
     });
 
 }());
--- dicamu/src/js/controllers/main.controller.js
+++ dicamu/src/js/controllers/main.controller.js
@@ -2,4 +2,5 @@ var app = angular.module("dcm");
 
 app.controller("MainController", function ($scope) {
     $scope.controllername = 'MainCtrl';
+    $scope.museums = "all the museums";
 });
