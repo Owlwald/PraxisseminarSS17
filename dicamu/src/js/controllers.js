a646a846eeebb75da12fc5475f774dcafaebc80a
Gina Maria Wolf <ginamariawolf@gmail.com>
2017-09-08 00:04:48 +0200

    wtf

--- dicamu/src/js/controllers.js
+++ dicamu/src/js/controllers.js
@@ -2,8 +2,23 @@
     'use strict';
     var app = angular.module("dcm");
 
-    app.controller("MainController", function ($scope) {
-        $scope.controllername = 'MainCtrl';
-        $scope.museums = "all the museums";
+    app.controller("ArtCtrl", function ($scope, $rootScope) {
+        $rootScope.topTitle = 'Museen';
+    });
+
+    app.controller("ArtListCtrl", function ($scope, $rootScope) {
+        $rootScope.topTitle = 'Museen';
+    });
+
+    app.controller("CatCtrl", function ($scope, $rootScope) {
+        $rootScope.topTitle = 'Museen';
+    });
+
+    app.controller("MyCatCtrl", function ($scope, $rootScope) {
+        $rootScope.topTitle = 'Museen';
+    });
+
+    app.controller("HomeCtrl", function ($scope, $rootScope) {
+        $rootScope.topTitle = 'Museen';
     });
 }());
