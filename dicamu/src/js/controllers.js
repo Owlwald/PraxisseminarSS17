cc9d82a872b37a1ccb778c214ff76044a70b6ff1
Gina Maria Wolf <ginamariawolf@gmail.com>
2017-09-08 00:10:47 +0200

    set all titles in controllers

--- dicamu/src/js/controllers.js
+++ dicamu/src/js/controllers.js
@@ -3,19 +3,19 @@
     var app = angular.module("dcm");
 
     app.controller("ArtCtrl", function ($scope, $rootScope) {
-        $rootScope.topTitle = 'Museen';
+        $rootScope.topTitle = 'Name Kunstwerk';
     });
 
     app.controller("ArtListCtrl", function ($scope, $rootScope) {
-        $rootScope.topTitle = 'Museen';
+        $rootScope.topTitle = 'Kunstwerke in Name Katalog';
     });
 
     app.controller("CatCtrl", function ($scope, $rootScope) {
-        $rootScope.topTitle = 'Museen';
+        $rootScope.topTitle = 'Name Katalog';
     });
 
     app.controller("MyCatCtrl", function ($scope, $rootScope) {
-        $rootScope.topTitle = 'Museen';
+        $rootScope.topTitle = 'Meine Kataloge';
     });
 
     app.controller("HomeCtrl", function ($scope, $rootScope) {
